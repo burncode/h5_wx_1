42868b2ce2cfb75a7112f3a619ab533757e7a74b
rumengkai <rumengkai@aliyun.com>
2017-11-30 18:46:37 +0800

    output

--- js/index.js
+++ js/index.js
@@ -22,12 +22,19 @@ music.onclick=function (event) {
     audio.play();
   }
 }
+
+
+
+
+
+
+//分享设置
 window.HOST='http://api.51xy8.com'
 window.shareData = {
-  title:"",
-  link:"",
-  imgUrl:"",
-  desc:"",
+  title:document.title,
+  link:location.href,
+  imgUrl:"http://m.51xy8.com/static/img_h5/h5_logo.png",
+  desc:"功夫财经",
 }
 console.log(isWeiXin());
 if (isWeiXin()) {
@@ -136,7 +143,22 @@ function weixinShare(data){
 	});
 }
 
-
+function browserRedirect() {
+  var sUserAgent = navigator.userAgent.toLowerCase();
+  var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+  var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+  var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+  var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+  var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+  var bIsAndroid = sUserAgent.match(/android/i) == "android";
+  var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+  var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+  if (bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 
 // window.onload=function () {
